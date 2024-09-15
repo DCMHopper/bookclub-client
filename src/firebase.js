0512a8041a90ad37e3a7d421df9ba7f5b124206c
DCMHopper <derekchristophermichaelhopper@gmail.com>
2024-09-15 18:15:23 -0400

    init

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAd2-gu6k7n62LAa_y97PUAfxkescG9LoY",
+
+  authDomain: "bookclub-34ca9.firebaseapp.com",
+
+  projectId: "bookclub-34ca9",
+
+  storageBucket: "bookclub-34ca9.appspot.com",
+
+  messagingSenderId: "41060787707",
+
+  appId: "1:41060787707:web:3062bb8b1546e68709575e"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
